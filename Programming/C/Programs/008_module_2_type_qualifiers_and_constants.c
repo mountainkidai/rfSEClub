5ef88a4677ecf1bba2318dbb4c3dc3024eccf90c
aarivtmk <aariv.tmk@gmail.com>
2025-09-25 18:40:15 +0530

    added upto type qualifiers

---
+++ Programming/C/Programs/008_module_2_type_qualifiers_and_constants.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#define PI 3.14159
+#define E 2.71828
+
+int main() {
+float radius = 5.0;
+printf("Area of circle with radius %6.2f: %.2f\n", radius, PI * radius * radius);
+printf("Value of e: %.5f\n", E);
+return 0;
+}
+
