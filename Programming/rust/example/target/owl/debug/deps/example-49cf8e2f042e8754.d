fcb390ae5e0512105d268b37a96acd2d43140fe7
aarivtmk <aariv.tmk@gmail.com>
2025-11-15 11:13:25 +0530

    added traits and generics code snippets to the example folder

--- Programming/rust/example/target/owl/debug/deps/example-49cf8e2f042e8754.d
+++
@@ -1,5 +0,0 @@
-/Users/komalsambana/projects/opensource/rfSEClub/Programming/rust/example/target/owl/debug/deps/libexample-49cf8e2f042e8754.rmeta: src/main.rs
-
-/Users/komalsambana/projects/opensource/rfSEClub/Programming/rust/example/target/owl/debug/deps/example-49cf8e2f042e8754.d: src/main.rs
-
-src/main.rs:
--- Programming/rust/example/target/owl/debug/deps/example-bf9f53fe3b0b1d35.d
+++
@@ -1,5 +0,0 @@
-/Users/komalsambana/projects/opensource/rfSEClub/Programming/rust/example/target/owl/debug/deps/libexample-bf9f53fe3b0b1d35.rmeta: src/main.rs
-
-/Users/komalsambana/projects/opensource/rfSEClub/Programming/rust/example/target/owl/debug/deps/example-bf9f53fe3b0b1d35.d: src/main.rs
-
-src/main.rs:
